becf88964ec74a492830a910192b9a4c1a4c32c5
AndOne1shot <wnduddud97@naver.com>
2025-01-07 10:43:52 +0900

    카드 회전하는 css추가

--- src/PokemonCards.js
+++ src/PokemonCards.js
@@ -1,7 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
+function PokemonCard({ card }) {
+    const [rotation, setRotation] = useState({ x: 0, y: 0 });
+
+    const handleMouseMove = useCallback((e) => {
+        const rect = e.currentTarget.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+        const rotateY = -2/15 * x + 20;
+        const rotateX = 1/15 * y - 20;
+
+        setRotation({ x: rotateX, y: rotateY });
+    }, []);
+
+    const handleMouseLeave = useCallback(() => {
+        setRotation({ x: 0, y: 0 });
+    }, []);
+
+    return (
+        <Link to={`/card/${card.id}`}>
+            <div 
+                className='container'
+                onMouseMove={handleMouseMove}
+                onMouseLeave={handleMouseLeave}
+                style={{
+                    transform: `perspective(400px) rotateX(${rotation.x}deg) rotateY(${rotation.y}deg)`,
+                    transition: 'transform 0.1s'
+                }}
+            >
+                <img src={card.images.small} alt={card.name} style={{ width: '100%' }} className='card' />
+            </div>
+        </Link>
+    );
+}
 
 function PokemonCards() {
     const [cards, setCards] = useState([]);
@@ -31,11 +64,7 @@ function PokemonCards() {
     return (
         <div className="flex-container">
             {cards.map(card => (
-                <Link key={card.id} to={`/card/${card.id}`} className="detail">
-                    <div>
-                        <img src={card.images.small} alt={card.name} style={{ width: '100%' }} />
-                    </div>
-                </Link>
+                <PokemonCard key={card.id} card={card} />
             ))}
         </div>
     );
